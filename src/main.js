1bcd3c3a0413328cc83e104a6de3533d6ffd5b5b
danipreciado <danielapca96@gmail.com>
2023-03-24 06:39:08 -0700

    finished main page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { example } from './data.js';
+import { example } from './data/ghibli/ghibli.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+
+const animationsBtn = document.getElementById
+
+
