e6a4866c9f6123048b44e867d7054fde4c92c044
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-03-27 10:58:05 -0300

    Subiendo cambios del botón personajes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,52 @@
-import { example } from './data/ghibli/ghibli.js';
-// import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
 
 
-const animationsBtn = document.getElementById
+/* const films = data.films;
+const filmsBtn = document.getElementById("filmsBtn");
+const charactersBtn = document.getElementById("charactersBtn");
+const mainElement =  document.getElementById("main");
+const walpaper = document.getElementById("body");
 
 
+filmsBtn.addEventListener("click",showMovies);
+
+function showMovies(event){
+  event.preventDefault();
+  walpaper.style.backgroundImage = "url(images/forest-background.jpg)";
+
+  mainElement.innerHTML = films.map((pelicula) => `
+
+  <div class="poster">
+  
+    <img src="`+ pelicula.poster + `" />
+      
+  </div>
+
+`);
+}
+
+charactersBtn.addEventListener("click",showCharacters);
+
+function showCharacters(event){
+  walpaper.style.backgroundImage = "url(images/forest-background.jpg)";
+  event.preventDefault();
+
+  mainElement.innerHTML = films.map((movie) => movie.people.map(character => `
+
+    <div class="people">
+    
+    <img src="`+ character.img + `" />
+    
+    <div class="overlay">
+    <h3>Name: `+ character.name +`</h3>
+    <h3>Gender: `+ character.gender +`</h3>
+    <h3>Age: `+ character.age +`</h3>
+    <h3>Eye color: `+ character.eye_color +`</h3>
+    <h3>Hair color: `+ character.hair_color +`</h3>
+    <h3>Specie: `+ character.specie +`</h3>
+    </div>
+        
+    </div>
+
+`));
+} */
