1b8cfee7fd98f6d93ab67a0c040edb00670e7c1e
4lb-ita <honnneygi@gmail.com>
2023-03-27 08:05:41 -0600

    cambios en imágenes, en index.htm, en style.css, se añadió novato.html

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+
+/*const buttonNewbie = document.getElementById("newbieBtn")
+buttonE.addEventListener("click", ()=>{
+  document.getElementById("resulted").innerHTML = cipher.encode(parseInt(offset.value),string.value);
+});*/
