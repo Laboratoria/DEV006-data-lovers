caa3991ca7785cd1770d9068532d719087a1d5d2
alison=leodra <alison.le.saa@gmail.com>
2023-03-29 14:32:45 -0300

    pruebas imagenes

--- src/main.js
+++ src/main.js
@@ -6,5 +6,22 @@ import data from './data/ghibli/ghibli.js';
 
 console.log(dataFilter, sortData, stats, data);
 
-data.films.forEach(element => 
-    console.log(element));
+/* const pelis = data.films
+    .map(element => element.poster)
+    .map(element => element + "-------------------")
+    .map(url => url.replaceAll("/","<3"))
+    .filter(url => url.includes("My")); */
+
+/*data.films
+    .map(element => element.poster)
+    .filter(url => url.includes("https"))
+    .map(url => createImage(url))
+    .forEach(image => document.getElementById("filmsZone").appendChild(image))
+
+function createImage(uri){
+    const image = document.createElement('img');
+    image.src = uri
+    image.className = "imagenes";
+    return image
+}
+*/
