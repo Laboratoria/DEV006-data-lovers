8208ca9d02b80673e18c0318c889d0b9d283a88c
alison=leodra <alison.le.saa@gmail.com>
2023-03-31 22:38:33 -0300

    imagen y titulo en pantalla

--- src/main.js
+++ src/main.js
@@ -6,22 +6,37 @@ import data from './data/ghibli/ghibli.js';
 
 console.log(dataFilter, sortData, stats, data);
 
-/* const pelis = data.films
-    .map(element => element.poster)
-    .map(element => element + "-------------------")
-    .map(url => url.replaceAll("/","<3"))
-    .filter(url => url.includes("My")); */
+class titleAndPoster {
+    constructor(title, poster) {
+        this.poster = createImage(poster);
+        this.title = createTitle(title);
 
-/*data.films
-    .map(element => element.poster)
-    .filter(url => url.includes("https"))
-    .map(url => createImage(url))
-    .forEach(image => document.getElementById("filmsZone").appendChild(image))
+    }
+}
+
+data.films
+    .map(film => new titleAndPoster(film.title,film.poster))
+    .map(Element => divCreator(Element))
+    .forEach(movieDiv =>document.getElementById("filmsZone").appendChild(movieDiv))
+
+function divCreator(x){
+    const movieDiv = document.createElement("div");
+    movieDiv.appendChild(x.title);
+    movieDiv.appendChild(x.poster);
+    return movieDiv
+}
 
 function createImage(uri){
-    const image = document.createElement('img');
-    image.src = uri
-    image.className = "imagenes";
-    return image
+        const image = document.createElement('img');
+        image.src = uri
+        image.className = "images";
+        return image
 }
-*/
+
+function createTitle(titleMovie){
+    const title = document.createElement("p");
+    title.innerText = titleMovie;
+    title.className = "titles";
+    return title
+}
+
