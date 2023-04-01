b8dfce04487cd23027573f3fc312cb219acebeb0
RisellU <rcum22@gmail.com>
2023-04-01 09:45:56 -0500

    imagenes y nombres

--- src/data/lol/lol.js
+++ src/data/lol/lol.js
@@ -6840,3 +6840,4 @@ export default {
     }
   }
 };
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/lol/lol.js';
 
-console.log(example, data);
+const contenedor = document.querySelector('.contenedor'); 
+
+const champion = Object.keys(data.data)
+
+for(let i = 0; i < champion.length; i++){
+  const key = champion[i]
+  
+  const name = data.data[key].name;
+  const img  = data.data[key].img
+  const championCard = document.createElement("div");
+  championCard.classList.add("champion-card"); 
+
+  // Creación div y clase para img verde de fondo
+  const championImgContainer = document.createElement("div");
+  championImgContainer.classList.add("champion-img-container");
+
+  const championImg = document.createElement("img");
+  championImg.src = img;
+  championImg.alt = name;
+
+  // creación h2 para ingresar nombre
+  const championName = document.createElement("h2");
+  // toma el primer caracter del nombre para que sea en mayuscula y juntarlo con slice para devolver el resto de caracteres.
+  championName.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+
+  championImgContainer.appendChild(championImg);
+  championCard.appendChild(championImgContainer)
+  championCard.appendChild(championName)
+  contenedor.appendChild(championCard)
+}
