b37a8ef1ee8802775c86a77f293f2d30672cee6d
alison=leodra <alison.le.saa@gmail.com>
2023-04-03 11:34:50 -0400

    imagen con informacion por pantalla

--- src/main.js
+++ src/main.js
@@ -7,25 +7,41 @@ import data from './data/ghibli/ghibli.js';
 console.log(dataFilter, sortData, stats, data);
 
 class titleAndPoster {
-    constructor(title, poster) {
+    constructor(title, poster,director,release_date,rt_score,description) {
         this.poster = createImage(poster);
         this.title = createTitle(title);
+        this.director = createTitle(director);
+        this.release_date = createTitle(release_date);
+        this.rt_score = createTitle(rt_score);
+        this.description = createTitle(description);
 
     }
 }
 
 data.films
-    .map(film => new titleAndPoster(film.title,film.poster))
+    .map(film => new titleAndPoster(film.title,film.poster,film.director,film.release_date,film.rt_score,film.description))
     .map(Element => divCreator(Element))
     .forEach(movieDiv =>document.getElementById("filmsZone").appendChild(movieDiv))
 
 function divCreator(x){
     const movieDiv = document.createElement("div");
     movieDiv.className = "filmDivs"
-    movieDiv.appendChild(x.title);
     movieDiv.appendChild(x.poster);
+    movieDiv.appendChild(divCreatorhover(x))
     return movieDiv
 }
+
+function divCreatorhover(x){
+    const movieDivHover = document.createElement("div");
+    movieDivHover.className = "filmDivsHover"
+    movieDivHover.appendChild(x.title);
+    movieDivHover.appendChild(x.director);
+    movieDivHover.appendChild(x.release_date);
+    movieDivHover.appendChild(x.rt_score);
+    movieDivHover.appendChild(x.description);
+    return movieDivHover
+}
+
 
 function createImage(uri){
         const image = document.createElement('img');
@@ -37,7 +53,9 @@ function createImage(uri){
 function createTitle(titleMovie){
     const title = document.createElement("p");
     title.innerText = titleMovie;
-    title.className = "titles";
+    title.className =  "titles";
     return title
 }
+
+
 
