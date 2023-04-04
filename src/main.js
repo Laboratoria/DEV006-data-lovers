6a3462225d96922ed959f1046d6e99fa6f9d270b
RisellU <rcum22@gmail.com>
2023-04-04 12:05:58 -0500

    visualización de tarjetas, animación y blurb

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ for(let i = 0; i < champion.length; i++){
   const key = champion[i]
   
   const name = data.data[key].name;
-  const img  = data.data[key].img
+  const img  = data.data[key].img;
+  const blurb = data.data[key].blurb;
+  //const info = data.data[key].info;
+  //console.log(info)
+
   const championCard = document.createElement("div");
   championCard.classList.add("champion-card"); 
 
@@ -18,14 +22,27 @@ for(let i = 0; i < champion.length; i++){
   const championImg = document.createElement("img");
   championImg.src = img;
   championImg.alt = name;
+  championImg.classList.add("champion-img");
 
   // creación h2 para ingresar nombre
   const championName = document.createElement("h2");
   // toma el primer caracter del nombre para que sea en mayuscula y juntarlo con slice para devolver el resto de caracteres.
   championName.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+  championName.classList.add("champion-name");
 
+  const championBlurb = document.createElement("p");
+  championBlurb.textContent = name.charAt(0).toUpperCase() + blurb.slice(1);
+  championBlurb.classList.add("blurb");
+  
+  //const championInfo = document.createElement("p")
+  //championInfo.textContent = name.charAt(0).toUpperCase() + info.slice(1);
+  //console.log(championInfo)
+  //championInfo.classList.add("info");
+  
   championImgContainer.appendChild(championImg);
-  championCard.appendChild(championImgContainer)
-  championCard.appendChild(championName)
-  contenedor.appendChild(championCard)
+  championCard.appendChild(championName);
+  championCard.appendChild(championImgContainer);
+  contenedor.appendChild(championCard);
+  championCard.appendChild(championBlurb);
+  //championCard.appendChild(championInfo);
 }
