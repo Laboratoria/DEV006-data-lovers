93b0346455f065ead36110ff85945dfb1f58343a
4lb-ita <honnneygi@gmail.com>
2023-04-04 21:33:52 -0600

    idioma inglés implementado. Método sort implementado en data.js. Adición de botones para el método sort en peliculas.html + adición de manejo de DOM en main.js para inicializar esas funciones. Se añadió un botón para refrescar la página (undo sort).

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-//import {"title"} from './ghibli.js';
-// import data from './data/lol/lol.js';
-//import data from './data/ghibli/ghibli.js';
 
-import {filterBy} from './data.js';
-
-//console.log(movieTitles);
-
+import {filterBy, sortByDate, sortByTitle} from './data.js';
 
 const directorSelect = document.getElementById("directorSelect");
 directorSelect.addEventListener("change", () => {
@@ -13,16 +7,17 @@ directorSelect.addEventListener("change", () => {
   console.log(directorSelect.value);
 });
 
+const sortByDateButton = document.getElementById("sortByDate")
+sortByDateButton.addEventListener("click", () => { 
+  sortByDate();
+});
 
+const sortByTitleButton = document.getElementById("sortByTitle")
+sortByTitleButton.addEventListener("click", () => { 
+  sortByTitle();
+});
 
-
-//const MoviePostersButton = document.getElementById("moviePostersBtn")
-//MoviePostersButton.addEventListener("click", ()=>{
-// document.getElementById("moviePosters").innerHTML = moviePosters;
-//});
-
-
-
-//const moviePosters = document.getElementsById("moviePosters").innerHTML = moviePosters;
-
-
+const buttonRefresh = document.getElementById("refresh")
+buttonRefresh.addEventListener("click",()=>{
+  history.go(0);
+});
