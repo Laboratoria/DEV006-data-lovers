83c656a26a0faa484556fc1699d39911a7de5e05
Dul <dul31389@gmail.com>
2023-04-05 12:19:13 -0500

    commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
+import{
+    
+}
+
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
