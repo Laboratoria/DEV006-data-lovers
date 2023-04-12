f172701f4279c073431568cd2f122b8a768280f9
Leonor <leonor.huanachin@gmail.com>
2023-04-12 10:21:28 -0500

    prueba1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+const backgrounds = [];
+backgrounds[0] = '../images/img1.jpeg';
+backgrounds[1] = '../images/img2.jpeg';
+backgrounds[2] = '../images/img3.jpeg';
+backgrounds[3] = '../images/img4.jpeg';
+backgrounds[4] = '../images/img5.jpeg';
+backgrounds[5] = '../images/img6.jpeg';
+backgrounds[6] = '../images/img7.jpeg';
+backgrounds[7] = '../images/img8.jpeg';
+
+window.onload = function (){
+  const setBackground = Math.floor(Math.random() * backgrounds.length);
+  document.body.style.backgroundImage = `url(${backgrounds[setBackground]})`;
+}
