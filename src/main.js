70c175b481e54c35ea492d09facefb99b69fbf66
Tamarindo <t.aviles.bosoni@gmail.com>
2023-04-11 20:26:29 -0400

    display data with image

--- src/main.js
+++ src/main.js
@@ -5,32 +5,38 @@ console.log(HP)
 
 let selectPerson = document.getElementById("selectPerson");
 const divCharacters = document.getElementById("charactersInfo");
+const housesLogo = './images/houses_logo.png';
 
-function filterByCharacters(characters, filter) {
+function filterByCharacters(characters) {
   return HP(dataHP.characters, characters);
 }
 
 function displayPersonajes(personajes) {
+  // la línea de abajo sirve para al realizar una nueva busqueda el contenido anterior se borre
+  divCharacters.innerHTML="" 
   personajes.forEach((characters) => {
     /* '+=' indica que el contenido anterior no se eliminará y 
     el contenido nuevo se agrega al final */
         /* '#$' se utiliza para insertar el valor de la propiedad id, name, etc.
      de cada objeto "characters" */ 
-    charactersInfo.innerHTML += `<div id="charactersInfo">
-        #${characters.id}
-        #${characters.name}
-        #${characters.birth}
-        #${characters.house}
+     divCharacters.innerHTML +=
+     `<div class="iDCard">
+     <img id="housesLogos" src="${housesLogo}" alt="housesLogo">
+     <ul>
+     <li><strong> ID: </strong> ${characters.id}</li>
+     <li><strong>NAME:</strong> ${characters.name}</li>
+     <li><strong>BIRTH:</strong> ${characters.birth}</li>
+     <li><strong>HOUSE</strong> ${characters.house}</li>
+     </ul>
       </div>`;
   })
 };
+displayPersonajes(dataHP.characters);
 
 selectPerson.addEventListener("input", () => {
-  let characters = selectPerson.value;
+  const characters = selectPerson.value;
   displayPersonajes(filterByCharacters(dataHP.characters, characters));
 });
-
-displayPersonajes(dataHP.characters);
  /*displayPersonajes(filterByCharacters(characters));*/
 
 
