5d3c952c655103885b5b3dd470ea51da0681436e
Dul <dul31389@gmail.com>
2023-04-11 19:59:29 -0500

    boton enter

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+const enter= document.getElementById('btn-enter');
+btn-enter.addEventListener('click', () =>{
+window.location.href="intro.html";
+});
+
 import {} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
