fdd6766fdbb717ef05cb6d5e106f17f92b17ee56
4lb-ita <honnneygi@gmail.com>
2023-04-13 09:51:05 -0600

    modificaciones en characters, data, fandom, index, main, novato y peliculas. Se añadieron vehiculos y locations

--- src/data.js
+++ src/data.js
@@ -2,55 +2,60 @@
 import data from './data/ghibli/ghibli.js';
 const dataFilms = data.films;
 
-const moviesTable = document.getElementById("moviesTable");
-const moviesBody = moviesTable.getElementsByTagName('tbody')[0];
-let tableBody = "";
 
-export function filterBy(director){
+export function filterByMovies(director) {
+  const moviesTable = document.getElementById("moviesTable");
+  if (!moviesTable) {
+    //console.error("moviesTable element not found");
+    return;
+  }
+  const moviesBody = moviesTable.getElementsByTagName('tbody')[0];
   let filteredFilms = dataFilms;
-  const chosenDirector = document.getElementById("directorSelect").value;
-  console.log(chosenDirector);
 
-  if (director !== "allDirectors"){
-    filteredFilms = dataFilms.filter(movie => movie.director === chosenDirector);
-  } 
+  if (director !== "allDirectors") {
+    filteredFilms = dataFilms.filter(movie => movie.director === director);
+  }
 
-  tableBody="";
+  let tableBody = "";
   filteredFilms.forEach((movie, index) => {
     if (index % 3 === 0) {
       tableBody = tableBody + "<tr>"
     }
     tableBody = tableBody + "<td>"
 
-    const posterImg = "<img src='" + movie.poster +"' class='moviePoster'/>"
+    const posterImg = "<img src='" + movie.poster + "' class='moviePoster'/>"
     const movieTitle = "<h3>" + movie.title + "</h3>";
-    const director = "<p> Director: " + movie.director + "</p>";
+    const director = "<p> Directed by " + movie.director + "</p>";
+    const producer = "<p> Produced by " + movie.producer + "</p>";
     const releaseDate = "<p>" + movie.release_date + "</p>";
     const description = "<p class='justify'>" + movie.description + "</p>";
-    
 
-    tableBody = tableBody + posterImg + movieTitle + director + releaseDate + description;
+    tableBody = tableBody + posterImg + movieTitle + director + producer + releaseDate + description;
     tableBody = tableBody + "</td>"
 
-    if(index % 3 === 2) {
+    if (index % 3 === 2) {
       tableBody = tableBody + "</tr>";
     }
   });
   moviesBody.innerHTML = tableBody;
 }
-filterBy("allDirectors");
+filterByMovies("allDirectors");
 
-export function sortMoviesByDate() {
-  dataFilms.sort((a, b) => (a.release_date) - (b.release_date));
-  filterBy("allDirectors");
-} 
-
-export function sortMoviesByTitle() {
-  dataFilms.sort((a, b) => (a.title).localeCompare(b.title));
-  filterBy("allDirectors");
+export function sortMoviesByDateNewestToOldest() {
+  dataFilms.sort((a, b) => (b.release_date) - (a.release_date));
 }
 
-console.log(new Date);
+export function sortMoviesByDateOldestToNewest() {
+  dataFilms.sort((a, b) => (a.release_date) - (b.release_date));
+}
 
-dataFilms.forEach(movie => console.log(movie.release_date));
-dataFilms.forEach(movie => console.log(movie.producer));
+
+
+/*export function sortMoviesByTitle() {
+  dataFilms.sort((a, b) => (a.title).localeCompare(b.title));
+}*/
+
+//console.log(new Date);
+
+//dataFilms.forEach(movie => console.log(movie.release_date));
+//dataFilms.forEach(movie => console.log(movie.producer));
--- src/main.js
+++ src/main.js
@@ -1,21 +1,64 @@
+import {filterByMovies, sortMoviesByDateNewestToOldest, sortMoviesByDateOldestToNewest} from './data.js';
+import {filterCharacters, filterCharactersBySpecie, sortCharactersByName} from './characters.js';
 
-import {filterBy, sortMoviesByDate, sortMoviesByTitle} from './data.js';
 
 const directorSelect = document.getElementById("directorSelect");
-directorSelect.addEventListener("change", () => {
-  filterBy(directorSelect.value);
-  console.log(directorSelect.value);
-});
+if (directorSelect) {
+  directorSelect.addEventListener("change", () => {
+    filterByMovies(directorSelect.value);
+    console.log(directorSelect.value);
+  });
+}
 
-const sortByDateButton = document.getElementById("sortMoviesByDateID");
-sortByDateButton.addEventListener("click", sortMoviesByDate);
+const sortByDateNOButton = document.getElementById("sortMoviesByDateNewestOldest");
+if (sortByDateNOButton) {
+  sortByDateNOButton.addEventListener("click", () => {
+    filterByMovies(directorSelect.value); 
+    sortMoviesByDateNewestToOldest();
+  });
+}
 
-const sortByTitleButton = document.getElementById("sortMoviesByTitleID")
-sortByTitleButton.addEventListener("click", () => { 
-  sortMoviesByTitle();
-});
+const sortByDateONButton = document.getElementById("sortMoviesByDateOldestNewest");
+if (sortByDateONButton) {
+  sortByDateONButton.addEventListener("click", () => {
+    filterByMovies(directorSelect.value);
+    sortMoviesByDateOldestToNewest();
+  });
+}
 
-const buttonRefresh = document.getElementById("refresh")
-buttonRefresh.addEventListener("click",()=>{
-  history.go(0);
+const buttonRefresh = document.getElementById("refresh");
+if (buttonRefresh) {
+  buttonRefresh.addEventListener("click",()=>{
+    history.go(0);
+  });
+}
+
+const filterGenderSelection = document.getElementById("genderSelect");
+if (filterGenderSelection) {
+  filterGenderSelection.addEventListener("change", () => {
+    filterCharacters(filterGenderSelection.value);
+    console.log(filterGenderSelection.value);
+  });
+}
+
+const filterSpecieSelection = document.getElementById("charactersSpecieSelect");
+if (filterSpecieSelection) {
+  filterSpecieSelection.addEventListener("change", () => {
+    filterCharactersBySpecie(filterSpecieSelection.value);
+    console.log(filterSpecieSelection.value);
+  });
+}
+
+const sortCharactersByNameButton = document.getElementById("sortCharactersByAge");
+if (sortCharactersByNameButton) {
+  sortCharactersByNameButton.addEventListener("click", () => {
+    filterGenderSelection(filterGenderSelection.value) +filterSpecieSelection(filterSpecieSelection.value);
+    sortCharactersByName();
+  });
+}
+
+import {filteredlocations} from './vehiculos.js';
+const climateSelect = document.querySelector('#climateSelect');
+climateSelect.addEventListener("change", () => {
+  filteredlocations();
 });
