dd2699203d033e57d8e5ad57b5edbefc38616416
Tamarindo <t.aviles.bosoni@gmail.com>
2023-04-13 21:54:48 -0400

    botones back&next fucnionales

--- src/data.js
+++ src/data.js
@@ -2,8 +2,8 @@ import dataHP from "./data/harrypotter/data.js";
 console.log(dataHP);
 
 export const HP = (dataHP, characters) => {
-  const filterByCharacters = dataHP.filter((harryPotter) => harryPotter.characters.includes(characters));
-  return filterByCharacters;
+  const callByCharacters = dataHP.filter((harryPotter) => harryPotter.characters.includes(characters));
+  return callByCharacters;
 }; 
 
 
--- src/main.js
+++ src/main.js
@@ -3,26 +3,26 @@ import { HP } from "./data.js";
 import dataHP from "./data/harrypotter/data.js";
 console.log(HP)
 
-let selectPerson = document.getElementById("selectPerson");
+const selectPerson = document.getElementById("selectPerson");
 const divCharacters = document.getElementById("charactersInfo");
 const housesLogo = './images/houses_logo.png';
 
 /* Llama a la data */
-function CallCharacters(characters) {
+function callCharacters(characters) {
   return HP(dataHP.characters, characters);
 }
 /* despliega la data */
 function displayPersonajes(personajes) {
-    // la línea de abajo sirve para al realizar una nueva busqueda el contenido anterior se borre
-    divCharacters.innerHTML = ""
-    personajes.forEach((characters) => {
-      /* '+=' indica que el contenido anterior no se eliminará y 
-      el contenido nuevo se agrega al final */
-      /* '$' se utiliza para insertar el valor de la propiedad id, name, etc.
-   de cada objeto "characters" */
-   /* las comillas inversas `` se utilizan para insertar valores de vbles dentro de una cadena de txto*/
-      divCharacters.innerHTML +=
-        `<div class="iDCard">
+  // la línea de abajo sirve para al realizar una nueva busqueda el contenido anterior se borre
+  divCharacters.innerHTML = ""
+  personajes.forEach((characters) => {
+    /* '+=' indica que el contenido anterior no se eliminará y 
+    el contenido nuevo se agrega al final */
+    /* '$' se utiliza para insertar el valor de la propiedad id, name, etc.
+ de cada objeto "characters" */
+    /* las comillas inversas `` se utilizan para insertar valores de vbles dentro de una cadena de txto*/
+    divCharacters.innerHTML +=
+      `<div class="iDCard">
      <img id="housesLogos" src="${housesLogo}" alt="housesLogo">
      <ul>
      <li><strong> ID: </strong> ${characters.id}</li>
@@ -31,35 +31,76 @@ function displayPersonajes(personajes) {
      <li><strong>HOUSE</strong> ${characters.house}</li>
      </ul>
       </div>`;
-    })
-  };
+  })
+};
 
 selectPerson.addEventListener("input", () => {
   const characters = selectPerson.value;
-  displayPersonajes(CallCharacters(dataHP.characters, characters));
+  displayPersonajes(callCharacters(dataHP.characters, characters));
 });
 
-let charactersPerPage= 42
-let page = 0
-let initialCharacter = page*charactersPerPage
-let finallCharacter= page*charactersPerPage+(charactersPerPage-1)
-let screenCharacters = dataHP.characters.slice(initialCharacter, finallCharacter)
+
+/*Personajes por página */
+const charactersPerPage = 42;
+let page = 0;
+const initialCharacter = page * charactersPerPage;
+const finalCharacter = page * charactersPerPage + charactersPerPage;
+const screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter);
 
 displayPersonajes(screenCharacters);
 
-document.getElementById("")
-function nextPage() {
-  
+const buttonNext = document.getElementById("next");
+buttonNext.addEventListener("click", nextPage);
+const buttonBack = document.getElementById("back");
+buttonBack.addEventListener("click", previousPage);
 
+function nextPage() {
+  /*Muestra 42 personajes a la vez*/
+  page = page + 1;
+  let initialCharacter = page * charactersPerPage
+  let finalCharacter = page * charactersPerPage + (charactersPerPage)
+  let screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter)
+  displayPersonajes(screenCharacters);
+  updateButtons();
+};
+
+function previousPage() {
+  /*Muestra 42 personajes a la vez*/
+  if (page > 0) {
+    page = page - 1;
+    let initialCharacter = page * charactersPerPage
+    let finalCharacter = page * charactersPerPage + (charactersPerPage)
+    let screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter)
+    displayPersonajes(screenCharacters);
+    updateButtons();
+  }
 }
 
+function updateButtons(){
+  
+const nextButton = document.querySelector("#next.backNext");
+const backButton = document.querySelector("#back.backNext");
+
+/*Quitar y agrega boton next acorde a necesidad */
+if (page == 16) {
+  nextButton.style.visibility = "hidden";
+} else {
+  nextButton.style.visibility = "visible";
+};
+  /*Quitar y agrega boton back acorde a necesidad */
+if (page == 0) {
+  backButton.style.visibility = "hidden";
+} else {
+  backButton.style.visibility = "visible";
+};
+};
+
+console.log("Page value outside function:", page);
 
 
 
 
 
-
-/*displayPersonajes(filterByCharacters(characters));*/
 
 
 /*btnAllTypes.addEventListener("click", function () {
