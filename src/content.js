f7fef43e18e6d8dd98597b289d15edc4e7ecd14a
Jae <one.sweet.dreams@gmail.com>
2023-04-13 21:29:29 -0400

    commit

---
+++ src/content.js
@@ -0,0 +1,24 @@
+export const header =/*`  <header class="encabezado">
+
+<div id="logo">
+
+  <img src="./img/riot-game-blanco-logo.png" alt="logo-riot" id="logo-riot">
+  
+  <img src="./img/lol-logo-chibi.png" alt="logo-lol" id="logo-lol">
+
+</div>
+
+<nav class="navegador">
+  <ul class="links">
+    <li><a href="#"></a>Eventos</li>
+    <li><a href="#"></a>Noticias</li>
+    <li><a href="#"></a>Campeones</li>
+    <li><a href="#"></a>Productos</li>
+  </ul>
+
+  <ul class="sesion">
+    <li><a href="#"></a>Inicio de sesion</li>
+  </ul>
+</nav>
+
+</header>`*/
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { example } from './data.js';
 
 import data from './data/lol/lol.js';
 
+/*import { encabezado } from '../content.js';
+export const Main = () =>{
+ const mainContainer = document.createElement(`div`);
+mainContainer.innerHTML = encabezado;  document.body.appendChild(mainContainer);
+}*/
 
 
 console.log(example, data);
