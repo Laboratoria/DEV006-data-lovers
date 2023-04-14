8306ac524e1c8eb22d85fe6f8f3b31ba46e278c5
shadia <127427913+shadiasw@users.noreply.github.com>
2023-04-14 09:32:53 -0500

    ultimo

--- src/characters.js
+++ src/characters.js
@@ -1,5 +1,10 @@
 import { getCharacters } from "./data.js";
 // getCharacters()
 
-const characters = getCharacters();
-console.log(characters);
+const characters = getCharacters()
+console.log(characters); {
+let body= ""
+for (let i = 0; i<characters.lenght; i++){
+    body += '<tr><td>${characters[i].id}</td></tr>'
+    }
+}
