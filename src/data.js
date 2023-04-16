531b6ff254ca56a68aa2f4fe2714094ec756539b
Dul <dul31389@gmail.com>
2023-04-16 12:06:29 -0500

    barra de menu

--- src/data.js
+++ src/data.js
@@ -1,16 +1,40 @@
-import data from "./data/pokemon/pokemon.js"
+import data from "./data/pokemon/pokemon.js";
+import {
+  searchPokByName,
+  orderAz,
+  orderZa,
+  searchPokByNumber,
+  filterByType,
+} from 'data.js'
 console.log(data)
 
+// estas funciones son de ejemplo
+
+export const example = () => {
+  return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
+
+
+
+
+const btnBuscarByName = document.getElementById('search');
+btnBuscarByName.addEventListener('click', () =>{
+})
+
+
 //buscar por nombre en minuscula
-export function searchPokByName(name, dataList) {
-  const result = dataList.pokemon.find(p => p.name.toLowerCase() === name.toLowerCase());
-  return result || null;
-}
-
-
+export const searchPokByName = (name, dataList) =>{
+  const searching = dataList.pokemon.find(p => p.name.toLowerCase() === name.toLowerCase());
+  return searching || null;
+};
 
 // función para ordenar alfabeticamente de la A-Z
 export const orderAz = (dataList) => {
+//creamos otra constante para guardar el resultado de la función
   const resultado = dataList.sort((a, b) => {
     if (a.name < b.name) return -1;
     if(a.name > b.name) return 1;
@@ -19,6 +43,12 @@ export const orderAz = (dataList) => {
   return resultado;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//función invertir orden de la Z a la A
+export const orderZa = (dataList) => {
+const result = dataList.sort((a, b) => {
+  if (a.name < b.name) return 1;
+  if(a.name > b.name) return -1;
+  else return 0;
+});
+return result;
 };
