54874b14a602e593bf50aad3cc540a158132485b
Melissa Aquijes <melissa.geral10@gmail.com>
2023-04-15 22:08:31 -0500

    Se agrego el main, contiene nuesta portada de descripción de la pelicula , titulo, y un video de la pelicula, se agrego al main.js un fetch con funciones para poder cargar nuestros posters.

--- src/data/ghibli/ghibli.js
+++ src/data/ghibli/ghibli.js
@@ -1,4 +1,4 @@
-export const data={
+export default {
   "studio": "Studio Ghibli Inc.",
   "films": [
     {
--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
-import {data} from './data/ghibli/ghibli.js';
+const portadaVideo = document.querySelector('.portadaVideo');
+const portadaInfo = document.querySelector('.portadaInfo');
+const titleLogoContainer = document.querySelector('.titleLogoContainer');
+const parrafoContainer = document.querySelector('.parrafoContainer');
+const postersContainer = document.querySelector('.postersContainer');
+
+fetch('./data/ghibli/ghibli.json')
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+  const dataStudioGhibli = data.films;
+  postersGhibli(dataStudioGhibli);
+});
+
+
+const postersGhibli = (ghibliData) => {
+
+  ghibliData.forEach((e) => {
+    const poster= document.createElement('img');
+    poster.setAttribute('src', e.poster)
+    poster.setAttribute('class', 'poster')
+
+    postersContainer.append(poster)
+
+        poster.addEventListener('click', () => {
+            const video = `<video src= ${e.info[0].video} loop autoplay>`
+            const parrafo = `<p class='parrafo'>${e.info[0].texto}</p>`
+            const titleLogo = `<img class= 'imgTitle' src= ${e.info[0].logoTitleUrl} />`
+
+            portadaVideo.innerHTML = video;
+            parrafoContainer.innerHTML = parrafo;
+            titleLogoContainer.innerHTML = titleLogo;
+        })
+
+  })
+}
+
 
 
