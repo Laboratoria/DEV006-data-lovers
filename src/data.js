81e884554502d2e7c3bbb8b867ae1ef56d6f811d
Tamarindo <t.aviles.bosoni@gmail.com>
2023-04-17 10:53:49 -0400

    filtrado

--- src/data.js
+++ src/data.js
@@ -1,9 +1,19 @@
 import dataHP from "./data/harrypotter/data.js";
 console.log(dataHP);
 
-export const HP = (dataHP, characters) => {
-  const callByCharacters = dataHP.filter((harryPotter) => harryPotter.characters.includes(characters));
+export const HP = (dataHP) => {
+  const callByCharacters = dataHP.characters;
   return callByCharacters;
-}; 
+};
 
+export const filterWizards = dataHP.characters.filter((characters) => {
+  return characters.species === "Human";
+});
 
+export const filterMuggles = dataHP.characters.filter((characters) => {
+  return characters.species === "Muggle";
+});
+
+export const filterGhosts = dataHP.characters.filter((characters) => {
+  return characters.species === "Human (formerly)";
+});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 /*Importar data*/
 import { HP } from "./data.js";
 import dataHP from "./data/harrypotter/data.js";
+import { filterWizards } from "./data.js";
+import { filterMuggles } from "./data.js";
+import { filterGhosts } from "./data.js";
 console.log(HP)
 
 const selectPerson = document.getElementById("selectPerson");
@@ -34,81 +37,86 @@ function displayPersonajes(personajes) {
   })
 };
 
-selectPerson.addEventListener("input", () => {
-  const characters = selectPerson.value;
-  displayPersonajes(callCharacters(dataHP.characters, characters));
-});
-
-
 /*Personajes por página */
 const charactersPerPage = 42;
 let page = 0;
-const initialCharacter = page * charactersPerPage;
-const finalCharacter = page * charactersPerPage + charactersPerPage;
-const screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter);
 
-displayPersonajes(screenCharacters);
-
+/*llamado de botones next y back*/
 const buttonNext = document.getElementById("next");
 buttonNext.addEventListener("click", nextPage);
+
 const buttonBack = document.getElementById("back");
 buttonBack.addEventListener("click", previousPage);
 
+/*declaración de la función getScreenCharacters*/
+function getScreenCharacters() {
+  const initialCharacter = page * charactersPerPage;
+  const finalCharacter = page * charactersPerPage + charactersPerPage;
+  const screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter);
+  return screenCharacters
+};
+
+displayPersonajes(getScreenCharacters());
+
 function nextPage() {
   /*Muestra 42 personajes a la vez*/
-  page = page + 1;
-  let initialCharacter = page * charactersPerPage
-  let finalCharacter = page * charactersPerPage + (charactersPerPage)
-  let screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter)
-  displayPersonajes(screenCharacters);
+  page++;
+  displayPersonajes(getScreenCharacters());
   updateButtons();
 };
 
 function previousPage() {
   /*Muestra 42 personajes a la vez*/
   if (page > 0) {
-    page = page - 1;
-    let initialCharacter = page * charactersPerPage
-    let finalCharacter = page * charactersPerPage + (charactersPerPage)
-    let screenCharacters = dataHP.characters.slice(initialCharacter, finalCharacter)
-    displayPersonajes(screenCharacters);
+    page--;
+    displayPersonajes(getScreenCharacters());
     updateButtons();
   }
 }
 
-function updateButtons(){
-  
-const nextButton = document.querySelector("#next.backNext");
-const backButton = document.querySelector("#back.backNext");
+function updateButtons() {
+  const nextButton = document.querySelector("#next");
+  const backButton = document.querySelector("#back");
 
-/*Quitar y agrega boton next acorde a necesidad */
-if (page == 16) {
-  nextButton.style.visibility = "hidden";
-} else {
-  nextButton.style.visibility = "visible";
-};
+  /*Quitar y agrega boton next acorde a necesidad */
+  if (page == 16) {
+    nextButton.style.visibility = "hidden";
+  } else {
+    nextButton.style.visibility = "visible";
+  };
+
   /*Quitar y agrega boton back acorde a necesidad */
-if (page == 0) {
-  backButton.style.visibility = "hidden";
-} else {
-  backButton.style.visibility = "visible";
-};
+  if (page == 0) {
+    backButton.style.visibility = "hidden";
+  } else {
+    backButton.style.visibility = "visible";
+  };
 };
 
 console.log("Page value outside function:", page);
 
+/*Filtro de personajes*/
+const callFilterForCharachters = document.getElementById("selectPerson");
+callFilterForCharachters.addEventListener("change", filterByTypeOfPerson);
 
+function filterByTypeOfPerson() {
+  const selectPersonValue = selectPerson.value;
+  
+  switch (selectPersonValue) {
+    case "magos":
+      displayPersonajes = filterWizards;
+      break;
 
+    case "muggles":
+      displayPersonajes = filterMuggles
+      break;
 
+    case "fantasmas":
+      displayPersonajes = filterGhosts
+      break;
 
-
-
-/*btnAllTypes.addEventListener("click", function () {
- pokemonDiv.innerHTML = "";
- mostrarPokemon(data.pokemon);
- */
-/*let charactersInfo = document.getElementById("charactersInfo")
-let characterId= CharacterData.[1];*/
-
-
-
+    default:
+      displayPersonajes = dataHP.characters;
+  };
+  callCharacters(dataHP.characters);
+};
