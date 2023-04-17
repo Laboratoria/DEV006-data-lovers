67766ba63d091310c69fb55497446b4692a65190
MontserratPoblano <christianpoblano@gmail.com>
2023-04-17 10:58:09 -0600

    modal funcionando en su totalidad

--- src/data.js
+++ src/data.js
@@ -15,26 +15,26 @@ export const anotherExample = () => {
 export  function mostrarPokemon(data) {
   const filterByType = data.length > 0 ? data: Objetos.pokemon
 
-  filterByType.forEach(pokemones => {
+  filterByType.forEach(pokemon => {
  const div = document.createElement('div');
  div.classList.add('pokemon');
  div.innerHTML = `
  <div class="pokemonImagen">
-<img src="${pokemones.img}" alt="${pokemones.name}">
+<img src="${pokemon.img}" alt="${pokemon.name}">
 </div>
 <div class="pokemon-info">
 <div class="nombre-contenedor">
-   <p class="pokemon-id">${pokemones.num}</p>
-   <h2 class="pokemon-nombre">${pokemones.name}</h2>
+   <p class="pokemon-id">${pokemon.num}</p>
+   <h2 class="pokemon-nombre">${pokemon.name}</h2>
 </div>
 <div class="pokemon-tipos">
-   <p class="tipo">${pokemones.type}</p>
+   <p class="tipo">${pokemon.type}</p>
 </div>
 <div class="pokemon-medidas">
-   <p class="medida">${pokemones.size.height}</p>
-   <p class="medida">${pokemones.size.weight}</p>
+   <p class="medida">${pokemon.size.height}</p>
+   <p class="medida">${pokemon.size.weight}</p>
 </div>
-<button type="button" class="btn-modal" id="${pokemones.num}">Ver mas</button>
+<button type="button" class="btn-modal" id="${pokemon.num}">Ver mas</button>
 </div>
   
  `
@@ -45,21 +45,93 @@ export  function mostrarPokemon(data) {
  });
 
  const abrir=document.querySelectorAll(".btn-modal");
- const modal=document.querySelectorAll(".modal");
- const modalC=document.querySelectorAll(".contenedorModal");
- console.log(abrir)
+  const modalC=document.querySelector("#contentModal");
  
- abrir.forEach( btn=>btn.addEventListener("click", (e)=>{
+
+ abrir.forEach( (btn,index)=>btn.addEventListener("click", (e)=>{
    e.preventDefault();
+   
    modalC.style.opacity="1";
-   modal.style.visibility="visible";
+   modalC.style.visibility="visible";
+    const divDos = document.createElement("div");
+   
+    divDos.classList.add(".modal");
+    const modalHtml=`
+   
+   <div class="modal modal-close" >
+     <p class="close">X<p>
+     <div class="caja1">
+     <div class="imagen-pokemon">
+       <img src="${filterByType[index].img}">
+       <p class="nombrePokemon">${filterByType[index].name}</p>
+       <p class="numeroPokemon">${filterByType[index].num}</p>
+     </div>
+     <div class="infoGeneral">
+       <p><i class="fa-solid fa-circle"></i>${filterByType[index].about}</p>
+     </div>
+   </div>
+   <div class="caja-alineada">
+     <div class="baseDefensa">
+       <p class="ataque"><i class="fa-solid fa-circle"></i>${filterByType[index].stats["base-attack"]}</p>
+       <p class="defensa"><i class="fa-solid fa-circle"></i>${filterByType[index].stats["base-defense"]}</p>
+     </div>
+     <div class="resistant">
+     <h3>resistant</h3>
+     <p class="resistencia">${filterByType[index].resistant}</p>
+   </div>
+   <div class="weaknesses">
+   <h3>weaknesses</h3>
+   <p class="debilidad">${filterByType[index].weaknesses}</p>
+ </div>
+</div>
+<div class="caja-alineada2">
+   <div class="special-attack">
+ <h3>Special special-attack</h3>
+ <ul>
+ <li>${filterByType[index]["special-attack"][0].name}</li>
+ <li>${filterByType[index]["special-attack"][0].type}</li>
+ <li>${filterByType[index]["special-attack"][0]["base-damage"]}</li>
+ <li>${filterByType[index]["special-attack"][0].energy}</li>
+ <li>${filterByType[index]["special-attack"][0]["move-duration-seg"]}</li>
+ </ul>
+</div>
+ <div class="evolution">
+   <h3>Evolucion</h3>
+   <p class="evolucion">${filterByType[index].evolution.candy}</p>
+ </div>
+</div>
+ </div>
+ </div>
+   `
+   
+   divDos.innerHTML = modalHtml
+   contentModal.append(divDos);
+
+
+   const modal=document.querySelectorAll(".modal")[0];
+   const cerrarModal=document.querySelector(".close");
    modal.classList.toggle("modal-close");
    
-   }))
+   cerrarModal.addEventListener("click", (e)=>{
+    divDos.innerHTML="";
 
+    modal.classList.toggle("modal-close");
+    
+    modalC.style.opacity="0";
+    modalC.style.visibility="hidden";
+    
+  
+  });
 
   
+   }));
+
+   
+   
+   
   };
+
+
 
 export function filtrarTipo(event) {
  const botonId = event.currentTarget.id;
@@ -67,8 +139,30 @@ export function filtrarTipo(event) {
 listaPokemon.innerHTML = " ";
  const tipos = Objetos.pokemon.filter(pokemon => pokemon.type.includes(botonId))
  mostrarPokemon(tipos);
+ 
 } 
 
 
+export function ordenar(){
+  listaPokemon.innerHTML = " ";
+ var mapped = Objetos.pokemon.map(function(el, i) {
+    return { index: i, value: el.name };
+  })
 
+   mapped.sort(function(a, b) {
+    if (a.value > b.value) {
+      return 1;
+    }
+    if (a.value < b.value) {
+      return -1;
+    }
+    return 0;
+  })
 
+var result = mapped.map(function(el){
+  return Objetos.pokemon[el.index];
+});
+
+ mostrarPokemon(result)
+
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filtrarTipo, mostrarPokemon } from './data.js';
+import { filtrarTipo, mostrarPokemon, ordenar } from './data.js';
 // import data from './data/lol/lol.js';
 import {Objetos} from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
@@ -6,13 +6,16 @@ import {Objetos} from './data/pokemon/pokemon.js';
 const listaPokemon = document.querySelector('#listaPokemon');
 const btnFiltro = document.querySelectorAll('.filtro-tipo');
 const btnData = document.getElementById('ver-todos');
+const btnAz=document.getElementById('filtrarAz');
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', mostrarPokemon); 
 btnData.addEventListener('click', mostrarPokemon);
 btnFiltro.forEach(boton => boton.addEventListener ('click', filtrarTipo));
-const cerrarModal=document.querySelectorAll(".close");
-
+btnAz.addEventListener("click", ordenar);
 
 
 
