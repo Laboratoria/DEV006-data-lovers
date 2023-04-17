45ad64ea4971bde67adf4bebade2439ddd4182f6
Andrea Roa <andrearoa182@gmail.com>
2023-04-17 09:25:56 -0500

    se agrega el filtro

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 /*Importar data*/
 import { HP } from "./data.js";
 import dataHP from "./data/harrypotter/data.js";
-console.log(HP);
+import { filterWizards } from "./data.js";
+// console.log(filterMuggles);
+import { filterMuggles } from "./data.js";
+// console.log(filterWizards);
+import { filterGhosts } from "./data.js";
+// console.log(filterWizards);
 
-let selectPerson = document.getElementById("selectPerson");
+const selectPerson = document.getElementById("selectPerson");
 const divCharacters = document.getElementById("charactersInfo");
 const housesLogo = "./images/houses_logo.png";
 
@@ -13,10 +18,11 @@ function CallCharacters(characters) {
 }
 /* despliega la data */
 function displayPersonajes(personajes) {
+  // console.log(personajes);
   // la línea de abajo sirve para al realizar una nueva busqueda el contenido anterior se borre
   divCharacters.innerHTML = "";
   personajes.forEach((characters) => {
-    /* '+=' indica que el contenido anterior no se eliminará y 
+    /* forEach es para que realice la funcion por cada elemento del array '+=' indica que el contenido anterior no se eliminará y 
       el contenido nuevo se agrega al final */
     /* '$' se utiliza para insertar el valor de la propiedad id, name, etc.
    de cada objeto "characters" */
@@ -33,24 +39,113 @@ function displayPersonajes(personajes) {
   });
 }
 
-selectPerson.addEventListener("input", () => {
+/*selectPerson.addEventListener("input", () => {
   const characters = selectPerson.value;
   displayPersonajes(CallCharacters(dataHP.characters, characters));
-});
+ //console.log("prueba:",CallCharacters(dataHP.characters, characters))
+  updateButtons();
+});*/
 
-let charactersPerPage = 42;
+/*Personajes por página */
+const charactersPerPage = 42;
 let page = 0;
-let initialCharacter = page * charactersPerPage;
-let finallCharacter = page * charactersPerPage + (charactersPerPage - 1);
-let screenCharacters = dataHP.characters.slice(
+const initialCharacter = page * charactersPerPage;
+const finalCharacter = page * charactersPerPage + charactersPerPage;
+const screenCharacters = dataHP.characters.slice(
   initialCharacter,
-  finallCharacter
+  finalCharacter
 );
 
 displayPersonajes(screenCharacters);
+/*button next: se indica que al momento de clickear de nuevo me tome el clickeado y me haga formula para mostrar data actualizada*/
 
-document.getElementById("");
-function nextPage() {}
+document.getElementById("next").addEventListener("click", buttonNext);
+document.getElementById("back").addEventListener("click", buttonBack);
+function buttonNext() {
+  page++;
+  const initialCharacter = page * charactersPerPage;
+  const finalCharacter = page * charactersPerPage + charactersPerPage;
+  const screenCharacters = dataHP.characters.slice(
+    initialCharacter,
+    finalCharacter
+  );
+  updateButtons();
+  //console.log(screenCharacters);
+  displayPersonajes(screenCharacters);
+}
+/*button back: */
+
+function buttonBack() {
+  if (page > 0) {
+    page = page - 1;
+    const initialCharacter = page * charactersPerPage;
+    const finalCharacter = page * charactersPerPage + charactersPerPage;
+    const screenCharacters = dataHP.characters.slice(
+      initialCharacter,
+      finalCharacter
+    );
+    updateButtons();
+    displayPersonajes(screenCharacters);
+  }
+}
+
+function updateButtons() {
+  const nextButton = document.querySelector("#next,backNext");
+  const backButton = document.querySelector("#back,backNext");
+
+  if (page === 16) {
+    nextButton.style.visibility = "hidden";
+  } else {
+    nextButton.style.visibility = "visible";
+  }
+
+  if (page === 0) {
+    backButton.style.visibility = "hidden";
+  } else {
+    backButton.style.visibility = "visible";
+  }
+}
+
+/*FIILTRAR personajes */
+const callFilterForCharacters = document.getElementById("selectPerson");
+callFilterForCharacters.addEventListener("change", filterByTypeOfPerson);
+function filterByTypeOfPerson() {
+  const selectPersonValue=selectPerson.value;
+  console.log(selectPersonValue)
+  switch (selectPersonValue) {
+  case "magos":
+    displayPersonajes(filterWizards);
+    console.log(filterWizards)
+    break;
+
+  case "muggles":
+    displayPersonajes(filterMuggles);
+    console.log(filterMuggles)
+    break;
+
+  case "fantasmas":
+    displayPersonajes(filterGhosts);
+    console.log(filterGhosts)
+    break;
+
+  default:
+    CallCharacters(dataHP.characters);
+  }
+}
+
+/*const selectedGender = event.target.value
+  if (selectedGender === "male") {
+    console.log(filterMale);
+    displayPersonajes(filterMale);
+  }
+  if (selectedGender === "female") {
+    console.log(filterFemale);
+    displayPersonajes(filterFemale);
+  }
+  if (selectedGender === "all") {
+    console.log(dataHP.characters);
+    displayPersonajes(dataHP.characters);
+  }*/
 
 /*displayPersonajes(filterByCharacters(characters));*/
 
