db9abfe56b706ad57cb59de6a28ce84be74c2b5e
Leonor <leonor.huanachin@gmail.com>
2023-04-19 20:27:56 -0500

    change8

--- src/characters.js
+++ src/characters.js
@@ -2,9 +2,9 @@ import { getCharacters } from "./data.js";
 // getCharacters()
 
 const characters = getCharacters();
-console.log(characters);
+//console.log(characters);
 const container = document.querySelector(".container");
-console.log(container);
+//console.log(container);
 
 const elements = [];
 for (let i=0;i<characters.length; i++){
@@ -37,6 +37,7 @@ container.innerHTML = elements.join("");
 ); */
 
 const searchInput = document.getElementById("search-input");
+//Funcion anomima / Pasar data.js
 searchInput.addEventListener("input", () => {
   const query = searchInput.value.toLowerCase();
   const filteredCharacters = characters.filter(character =>
@@ -57,6 +58,113 @@ function renderCharacters(characters) {
     </div>
   `).join("");
 }
-//renderCharacters(characters);
-    
 
+//Filter Human   
+const characterHuman= document.getElementById("Human");
+characterHuman.addEventListener("click", () => {
+  const filterHumanCharacters= characters.filter(character =>
+    character.species === "Human");
+  //console.log(filterHumanCharacters);
+  renderHumanCharacters(filterHumanCharacters);
+});
+function renderHumanCharacters(characters) {
+  container.innerHTML = characters.map(character => `
+    <div class="card"> 
+      <div class="card-body"> 
+        <a href=" "> <img src="${character.image}"></a>
+        <div>
+          <h5 class="card-title">${character.name}</h5>
+        </div>
+      </div>
+    </div>
+  `).join("");
+} 
+
+/*const characterGender= document.getElementById("Gender");
+characterGender.addEventListener("click", () => {
+  const filterMaleCharacters= characters.filter(character =>
+    character.gender === "Male"); */
+
+//Filter Male
+const characterMale= document.getElementById("Male");
+characterMale.addEventListener("click", () => {
+  const filterMaleCharacters= characters.filter(character =>
+    character.gender === "Male");
+  //console.log(filterMaleCharacters);
+  renderMaleCharacters(filterMaleCharacters);
+});
+function renderMaleCharacters(characters) {
+  container.innerHTML = characters.map(character => `
+    <div class="card"> 
+      <div class="card-body"> 
+        <a href=" "> <img src="${character.image}"></a>
+        <div>
+          <h5 class="card-title">${character.name}</h5>
+        </div>
+      </div>
+    </div>
+  `).join("");
+} 
+
+//Filter Female
+const characterFemale= document.getElementById("Female");
+characterFemale.addEventListener("click", () => {
+  const filterFemaleCharacters= characters.filter(character =>
+    character.gender === "Female");
+  //console.log(filterFemaleCharacters);
+  renderFemaleCharacters(filterFemaleCharacters);
+});
+function renderFemaleCharacters(characters) {
+  container.innerHTML = characters.map(character => `
+    <div class="card"> 
+      <div class="card-body"> 
+        <a href=" "> <img src="${character.image}"></a>
+        <div>
+          <h5 class="card-title">${character.name}</h5>
+        </div>
+      </div>
+    </div>
+  `).join("");
+} 
+
+//Filter Alive
+const characterAlive= document.getElementById("Alive");
+characterAlive.addEventListener("click", () => {
+  const filterAliveCharacters= characters.filter(character =>
+    character.status === "Alive");
+  //console.log(filterAliveCharacters);
+  renderAliveCharacters(filterAliveCharacters);
+});
+function renderAliveCharacters(characters) {
+  container.innerHTML = characters.map(character => `
+    <div class="card"> 
+      <div class="card-body"> 
+        <a href=" "> <img src="${character.image}"></a>
+        <div>
+          <h5 class="card-title">${character.name}</h5>
+        </div>
+      </div>
+    </div>
+  `).join("");
+} 
+
+//Filter Dead
+const characterDead= document.getElementById("Dead");
+characterDead.addEventListener("click", () => {
+  const filterDeadCharacters= characters.filter(character =>
+    character.status === "Alive");
+  //console.log(filterDeadCharacters);
+  renderDeadCharacters(filterDeadCharacters);
+});
+function renderDeadCharacters(characters) {
+  container.innerHTML = characters.map(character => `
+    <div class="card"> 
+      <div class="card-body"> 
+        <a href=" "> <img src="${character.image}"></a>
+        <div>
+          <h5 class="card-title">${character.name}</h5>
+        </div>
+      </div>
+    </div>
+  `).join("");
+} 
--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ backgrounds[1] = '../images/img2.jpeg';
 backgrounds[2] = '../images/img3.jpeg';
 backgrounds[3] = '../images/img4.jpeg';
 backgrounds[4] = '../images/img5.jpeg';
-backgrounds[5] = '../images/img6.jpeg';
-backgrounds[6] = '../images/img7.jpeg';
-backgrounds[7] = '../images/img8.jpeg';
+
+
 
 window.onload = function (){
   const setBackground = Math.floor(Math.random() * backgrounds.length);
