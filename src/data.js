c170782d6a630cbf73af6457835b346b11f39418
Dul <dul31389@gmail.com>
2023-04-20 19:38:30 -0500

    commit

--- src/data.js
+++ src/data.js
@@ -1,87 +1,71 @@
-import pokemon from "./data/pokemon/pokemon.js";
+//import pokemon from "./data/pokemon/pokemon.js";
 import data from "./data/pokemon/pokemon.js";
 console.log(data.pokemon)
-
+//En el archivo data.js, exporta las funciones de búsqueda y filtrado.
 
 //input SEARCH buscar por nombre 
-const btnBuscarByName = document.getElementById('search');
-btnBuscarByName.addEventListener('input', (e) =>{
-  searchPokByName(e.target.value)
-  console.log(e.target.value)
-  console.log(searchPokByName(e.target.value))
-}) 
-export const searchPokByName = (name) =>{
-//metodo startsWidth para buscar por nombre comenzando desde la primera
+export function searchPokByName(name) {
   const searching = data.pokemon.filter(p => p.name.startsWith(name));
-  return searching
-};
-
-
-//item order A-Z
-const orderAz = document.getElementById('az');
-orderAz.addEventListener('click', () => {
-  //ordenar la copia por nombre de la A hasta la Z
-  const ordenado = data.pokemon.sort((a,b)=> a.name.localeCompare(b.name));
-  //mostrar el resultado
+  return searching;
+}
+//ordenar de la A-Z
+const orderAz = document.getElementById("az");
+orderAz.addEventListener("click", () => {
+  const ordenado = data.pokemon.sort((a, b) => a.name.localeCompare(b.name));
   console.log(ordenado);
 });
 
-//item order Z-A
-const invertirZa = document.getElementById('za');
-invertirZa.addEventListener('click', () => {
-  const invertido = data.pokemon.reverse().sort((a,b)=> a.name.localeCompare(b.name));
-  //mostrar el resultado
+//ordenar de la Z-A
+const invertirZa = document.getElementById("za");
+invertirZa.addEventListener("click", () => {
+  const invertido = data.pokemon
+    .reverse()
+    .sort((a, b) => a.name.localeCompare(b.name));
   console.log(invertido);
 });
 
-//item order x NUMERO ascendente 
-const menorAmayor = document.getElementById('ascendente');
-menorAmayor.addEventListener('click', () => {
-  const ascending = data.pokemon.sort((a,b) => a.num - b.num);
+//ordenar ascendentemente
+const menorAmayor = document.getElementById("ascendente");
+menorAmayor.addEventListener("click", () => {
+  const ascending = data.pokemon.sort((a, b) => a.num - b.num);
   console.log(ascending);
 });
 
-
-//item order x NUMERO descendente
-const mayorAmenor = document.getElementById('descendente');
-mayorAmenor.addEventListener('click', () => {
-  const descending = data.pokemon.sort((a,b) => b.num - a.num);
+//ordenar descendente
+const mayorAmenor = document.getElementById("descendente");
+mayorAmenor.addEventListener("click", () => {
+  const descending = data.pokemon.sort((a, b) => b.num - a.num);
   console.log(descending);
 });
 
 
-//click en el boton filtrar y que vaya a la seccion de filtrar
-// Obtener el enlace "TO FILTER"
-const toFilterLink = document.getElementById('to-filter-link');
-
-// Agregar un evento de clic al enlace
-toFilterLink.addEventListener('click', (event) => {
-  // Prevenir el comportamiento predeterminado del enlace
-  event.preventDefault();
-  
-  // Obtener la sección correspondiente
-  const filtrandoSection = document.getElementById('filtrando-section');
-  
-  // Desplazarse suavemente hacia la sección correspondiente
-  filtrandoSection.scrollIntoView({ behavior: 'smooth' });
-});
-
-
-
-//Filtro por tipo
-const typeWater = document.getElementById('water');
-typeWater.addEventListener('click', () => {
-  filtradoTipo("water");
-})
-
-
-
-function filtradoTipo(type){
+//filtrado por tipo
+export function filtradoTipo(type) {
   const filtrado = data.pokemon.filter((pokemon) =>{
     if(pokemon.type.includes(type)){
       return pokemon;
     }
   });
-  console.log (filtrado);
+  return filtrado;
 }
 
+
+//filtar por debilidad
+export function filtradoDebilidad(weaknesses) {
+  const filtradoWeaknesses = data.pokemon.filter((pokemon) => {
+    if(pokemon.weaknesses.includes(weaknesses)){
+      return pokemon;
+    }
+  });
+  return filtradoWeaknesses;
+}
+
+//filtrar por fortaleza
+export function filtradoResistencia(resistant){
+  const filtradoResistant = data.pokemon.filter((pokemon)=> {
+    if(pokemon.resistant.includes(resistant)){
+      return pokemon;
+    }
+  });
+  return filtradoResistant;
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,46 @@
+import {searchPokByName, filtradoTipo, filtradoDebilidad, filtradoResistencia} from "./data.js";
+
+//main.js, importa las funciones exportadas de data.js y las usa para realizar la manipulación de dato
+//buscar por nombre
+const btnBuscarByName = document.getElementById("search");
+btnBuscarByName.addEventListener("input", (e) => {
+  const searchingResult = searchPokByName(e.target.value);
+  console.log(e.target.value);
+  console.log(searchingResult);
+});
+
+//para que al hacer click se baje a la seccion filtrando
+const toFilterLink = document.getElementById("to-filter-link");
+toFilterLink.addEventListener("click", (event) => {
+  event.preventDefault();
+  const filtrandoSection = document.getElementById("filtrando-section");
+  filtrandoSection.scrollIntoView({ behavior: "smooth" });
+});
+
+//filtrar por tipo
+const selectType = document.getElementById("element-type-filter");
+selectType.addEventListener("change", () => {
+  const filtrado = filtradoTipo(selectType.value);
+  console.log(filtrado);
+});
+
+//filtrar por debilidad
+const selectWeaknesses = document.getElementById("element-weaknesses-filter");
+selectWeaknesses.addEventListener("change", () => {
+  const filtradoD = filtradoDebilidad(selectWeaknesses.value);
+  console.log(filtradoD);
+});
+
+
+//filtrar por fortaleza
+const selectResistant = document.getElementById('element-resistant-filter');
+selectResistant.addEventListener("change", () => {
+  const filtradoR = filtradoResistencia(selectResistant.value);
+  console.log(filtradoR)
+});
+
+
+
 // estas funciones son de ejemplo
 export const example = () => {
   return 'example';
