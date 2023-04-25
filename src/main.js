66c918e15dfd1f112ef7092383fca22d2a89b684
mabeliris <mabelle7623@gmail.com>
2023-04-24 22:28:25 -0400

    iniciando estructura de html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { example } from './data.js';
+import main from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(main, data);
