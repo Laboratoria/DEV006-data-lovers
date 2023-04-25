325868ba8b5e565f46a042d5731d782c76f0af56
Tamarindo <t.aviles.bosoni@gmail.com>
2023-04-24 23:35:52 -0400

    porcentajes de cada casa

--- src/houses.js
+++ src/houses.js
@@ -19,8 +19,42 @@ window.onclick = function(event) {
   }
 
 
-/*Contador*/
-const countGryffindor = studentsPerProperty(charactersData, "house", "Gryffindor");
+/*Cantida de personajes por Casa*/
 
+/*Gryffindor*/
+const countGryffindor = studentsPerProperty(charactersData, "house", "Gryffindor");
+const totalStudentsG = countGryffindor.Gryffindor;
+console.log(totalStudentsG)
+/*Hufflepuff*/
+const countHufflepuff = studentsPerProperty(charactersData, "house", "Hufflepuff");
+const totalStudentsH = countHufflepuff.Hufflepuff;
+console.log(totalStudentsH)
+/*Ravenclaw*/
+const countRavenclaw = studentsPerProperty(charactersData, "house", "Ravenclaw");
+const totalStudentsR = countRavenclaw.Ravenclaw;
+console.log(totalStudentsR)
+/*Slytherin*/
+const countSlytherin = studentsPerProperty(charactersData, "house", "Slytherin");
+const totalStudentsS = countSlytherin.Slytherin;
+console.log(totalStudentsS)
+
+/*Contador*/
 const phouses = document.getElementById("housesInfo")
 phouses.innerHTML += `${countGryffindor}`
+
+
+/*Porcentajes*/
+const totalStudents = charactersData.length;
+console.log(totalStudents)
+/*Gryffindor*/
+const porcentajeG = ((totalStudentsG/totalStudents)*100).toFixed(2);
+console.log(parseFloat(porcentajeG)+ "%")
+/*Hufflepuff*/
+const porcentajeH = ((totalStudentsH/totalStudents)*100).toFixed(2);
+console.log(parseFloat(porcentajeH)+ "%")
+/*Ravenclaw*/
+const porcentajeR = ((totalStudentsR/totalStudents)*100).toFixed(2);
+console.log(parseFloat(porcentajeR)+ "%")
+/*Slytherin*/
+const porcentajeS = ((totalStudentsS/totalStudents)*100).toFixed(2);
+console.log(parseFloat(porcentajeS)+ "%")
