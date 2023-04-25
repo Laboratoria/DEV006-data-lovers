477396ed581f6a1ac350b35742cc05243c66cd91
Tamarindo <t.aviles.bosoni@gmail.com>
2023-04-24 22:57:33 -0400

    modal en gryffindor

--- src/houses.js
+++ src/houses.js
@@ -1,8 +1,26 @@
 import { charactersData } from "./data.js";
-import {studentsPerProperty} from "./data.js";
+import { studentsPerProperty } from "./data.js";
+
+/*Modal*/
+var modal = document.getElementById("myModal");
+var gryffindorBttn = document.getElementById("imageG");
+var span = document.getElementsByClassName("close")[0];
+
+gryffindorBttn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
 
 /*Contador*/
-const countByproperty = studentsPerProperty(charactersData, "house","Gryffindor");
-console.log(countByproperty);
+const countGryffindor = studentsPerProperty(charactersData, "house", "Gryffindor");
 
+const phouses = document.getElementById("housesInfo")
+phouses.innerHTML += `${countGryffindor}`
