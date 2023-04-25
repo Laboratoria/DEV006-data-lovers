62fcc5635cdd31140ceda88ea7006f447c8e520d
Tamarindo <t.aviles.bosoni@gmail.com>
2023-04-25 17:50:54 -0400

    intento fallido

--- src/houses.js
+++ src/houses.js
@@ -1,24 +1,6 @@
 import { charactersData } from "./data.js";
 import { studentsPerProperty } from "./data.js";
 
-/*Modal*/
-var modal = document.getElementById("myModal");
-var gryffindorBttn = document.getElementById("imageG");
-var span = document.getElementsByClassName("close")[0];
-
-gryffindorBttn.onclick = function () {
-    modal.style.display = "block";
-}
-span.onclick = function () {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
-
 /*Cantida de personajes por Casa*/
 
 /*Gryffindor*/
@@ -39,23 +21,68 @@ const totalStudentsS = countSlytherin.Slytherin;
 console.log(totalStudentsS)
 
 /*Porcentajes*/
+/*convierte string en número*/
 const totalStudents = charactersData.length;
 console.log(totalStudents)
 /*Gryffindor*/
-const porcentajeG = ((totalStudentsG/totalStudents)*100).toFixed(2);
-const resultadoG = parseFloat(porcentajeG)+ "%"
-console.log(resultadoG)
-/*Hufflepuff*/
-const porcentajeH = ((totalStudentsH/totalStudents)*100).toFixed(2);
-console.log(parseFloat(porcentajeH)+ "%")
-/*Ravenclaw*/
-const porcentajeR = ((totalStudentsR/totalStudents)*100).toFixed(2);
-console.log(parseFloat(porcentajeR)+ "%")
-/*Slytherin*/
-const porcentajeS = ((totalStudentsS/totalStudents)*100).toFixed(2);
-console.log(parseFloat(porcentajeS)+ "%")
+const porcentajeG = ((totalStudentsG / totalStudents) * 100).toFixed(2);
+const resultadoG = (porcentajeG) + "%"
 
-/*Contador*/
+/*Hufflepuff*/
+const porcentajeH = ((totalStudentsH / totalStudents) * 100).toFixed(2);
+const resultadoH = (porcentajeH) + "%"
+/*Ravenclaw*/
+const porcentajeR = ((totalStudentsR / totalStudents) * 100).toFixed(2);
+const resultadoR = (porcentajeR) + "%"
+/*Slytherin*/
+const porcentajeS = ((totalStudentsS / totalStudents) * 100).toFixed(2);
+const resultadoS = (porcentajeS) + "%"
+
+
+/*modal*/
+const modal = document.getElementById("myModal");
+const gryffindorBttn = document.getElementById("imageG");
+const hufflepuffBttn = document.getElementById("imageH");
+const ravenclawBttn = document.getElementById("imageR");
+const slytherinBttn = document.getElementById("imageS");
+const span = document.getElementsByClassName("close")[0];
+
+const modalHouses = (elementbtn) => {
+  if (elementbtn) {
+    elementbtn.onclick = function () {
+      modal.style.display = "block";
+    }
+  }
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+const modalG = modalHouses(gryffindorBttn);
+const modalH = modalHouses(hufflepuffBttn);
+const modalR = modalHouses(ravenclawBttn);
+const modalS = modalHouses(slytherinBttn);
+
 const phouses = document.getElementById("housesInfo")
-console.log(countGryffindor)
-phouses.innerHTML += `${resultadoG} ${totalStudentsG}`
+/*display info en modal*/
+phouses.innerHTML="";
+if (modalG) {
+  phouses.innerHTML+= `El total de estudiantes de Gryffindor es de <strong>${totalStudentsG}</strong>
+con un porcentaje de <strong>${resultadoG}<strong> `
+}
+else if (modalH){
+  phouses.innerHTML+= `El total de estudiantes de Hufflepuff es de <strong>${totalStudentsH}</strong>
+  con un porcentaje de <strong>${resultadoH}<strong> `
+}
+else if  (modalR){
+  phouses.innerHTML+=  `El total de estudiantes de RavenclawB es de <strong>${totalStudentsR}</strong>
+    con un porcentaje de <strong>${resultadoR}<strong> `
+}else {
+  phouses.innerHTML+= `El total de estudiantes de Slytherin es de <strong>${totalStudentsS}</strong>
+      con un porcentaje de <strong>${resultadoS}<strong> `
+}
+
