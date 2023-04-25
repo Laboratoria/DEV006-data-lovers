35502ad6dc2c03447d0355d8693c3d7fddb430ba
Leonor <leonor.huanachin@gmail.com>
2023-04-25 09:01:04 -0500

    change 10

--- src/data.js
+++ src/data.js
@@ -1,8 +1,44 @@
 import dataRickAndMorty from "./data/rickandmorty/rickandmorty.js"
 //console.log(dataRickAndMorty);
 
-
 export function getCharacters () {
   return dataRickAndMorty.results;
 }
-
+export function getCharactersByQuery(query){
+  return dataRickAndMorty.results.filter(character =>
+    character.name.toLowerCase().includes(query));
+}
+export function ordinationAz (dataCharacters){
+  return dataCharacters.sort((a,b)=>{
+    if(a.name < b.name){
+      return -1;
+    }
+    if (a.name > b.name){
+      return 1;
+    } 
+    return 0;
+  });
+}
+export function ordinationZa (dataCharacters){
+  return dataCharacters.sort((a,b)=>{
+    if(a.name > b.name){
+      return -1;
+    }
+    if (a.name < b.name){
+      return 1;
+    } 
+    return 0;
+  });
+}
+export function filterBySpecies (dataCharacters,species){
+  const speciesFilter= dataCharacters.filter((character)=> character.species === species);
+  return speciesFilter;
+}
+export function filterByGender (dataCharacters,gender){
+  const genderFilter= dataCharacters.filter((character)=> character.gender === gender);
+  return genderFilter;
+}
+export function filterByStatus(dataCharacters,status){
+  const statusFilter= dataCharacters.filter((character)=>character.status === status);
+  return statusFilter;
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-import { getCharacters } from "./data.js";
-getCharacters()
-
 const backgrounds = [];
 backgrounds[0] = '../images/img1.jpeg';
 backgrounds[1] = '../images/img2.jpeg';
@@ -8,10 +5,12 @@ backgrounds[2] = '../images/img3.jpeg';
 backgrounds[3] = '../images/img4.jpeg';
 backgrounds[4] = '../images/img5.jpeg';
 
-
-
-window.onload = function (){
+function setBackground(){
   const setBackground = Math.floor(Math.random() * backgrounds.length);
   document.body.style.backgroundImage = `url(${backgrounds[setBackground]})`;
 }
+
+window.onload = setBackground
+
+
 
