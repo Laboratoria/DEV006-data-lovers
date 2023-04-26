f562969cb32e62ddeee14fc8b9aab187f6eddda9
MontserratPoblano <christianpoblano@gmail.com>
2023-04-26 12:24:17 -0600

    Funciones completas y con test terminados y cambios en botones de card pokemon

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filtrarTipo, mostrarPokemon, ordenar,ordenarZa,busquedaName } from './data.js';
+import { mostrarPokemon, ordenar,ordenarZa,busquedaName,filtradoPorTipo } from './data.js';
 // import data from './data/lol/lol.js';
 import {Objetos} from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
@@ -6,34 +6,49 @@ import {Objetos} from './data/pokemon/pokemon.js';
 
 
 let pokemonPantalla=Objetos.pokemon
-console.log(pokemonPantalla)
-const listaPokemon = document.querySelector('#listaPokemon');
+
+
 const btnFiltro = document.querySelectorAll('.filtro-tipo');
 const btnData = document.getElementById('ver-todos');
 const btnAz=document.getElementById('filtrarAz');
 const btnZa=document.getElementById('filtrarZa');
-const inputBuscar=document.getElementById("busqueda")
+const listaPokemon = document.getElementById('listaPokemon');
 const iconobusqueda=document.getElementById("icono-busqueda");
 const navburger=document.querySelector(".nav-burger");
 const navMenu2=document.querySelector(".menu-nav2");
 navburger.addEventListener("click", ()=>
-navMenu2.classList.toggle("nav-menu_visible"));
+  navMenu2.classList.toggle("nav-menu_visible"));
 
   
 
 document.addEventListener('DOMContentLoaded', mostrarPokemon); 
 btnData.addEventListener('click', mostrarPokemon);
 btnFiltro.forEach(boton => boton.addEventListener ('click', (e)=> {
-    pokemonPantalla=filtrarTipo(e)
+
+  listaPokemon.innerHTML=" ";
+  const type=e.currentTarget.id
+  pokemonPantalla=filtradoPorTipo(Objetos.pokemon,type)
+  mostrarPokemon(pokemonPantalla)
 }));
 
-btnAz.addEventListener("click", (e)=>{
-    ordenar(pokemonPantalla)
+btnAz.addEventListener("click", ()=>{
+  listaPokemon.innerHTML=" ";
+  
+  const ordenarAzz= ordenar(pokemonPantalla) 
+  mostrarPokemon(ordenarAzz)
 });
-btnZa.addEventListener("click", (e)=>{
-    ordenarZa(pokemonPantalla)});
+
+btnZa.addEventListener("click", ()=>{
+
+  listaPokemon.innerHTML=" ";
+  const ordenarZaa= ordenarZa(pokemonPantalla) 
+  mostrarPokemon(ordenarZaa)
+});
 
 
 iconobusqueda.addEventListener("click", ()=>{
-pokemonPantalla=busquedaName()})
+  listaPokemon.innerHTML = " ";
+  pokemonPantalla=busquedaName();
+  mostrarPokemon(pokemonPantalla)
+})
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,225 @@
-import { example, anotherExample } from '../src/data.js';
+import { ordenar, filtradoPorTipo, ordenarZa } from '../src/data.js';
+
+const entrada = [{
+  "num": "004",
+  "name": "charmander",
+  "generation": {
+    "num": "generation i",
+    "name": "kanto"
+  },
+  "about": "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.",
+  "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+  "size": {
+    "height": "0.61 m",
+    "weight": "8.5 kg"
+  },
+  "pokemon-rarity": "normal",
+  "type": [
+    "fire"
+  ]
+},
+{
+  "num": "001",
+  "name": "bulbasaur",
+  "generation": {
+    "num": "generation i",
+    "name": "kanto"
+  },
+  "about": "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+  "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+  "size": {
+    "height": "0.71 m",
+    "weight": "6.9 kg"
+  },
+  "pokemon-rarity": "normal",
+  "type": [
+    "grass",
+    "poison"
+  ]
+},
+{
+  "num": "002",
+  "name": "ivysaur",
+  "generation": {
+    "num": "generation i",
+    "name": "kanto"
+  },
+  "about": "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
+  "img": "https://www.serebii.net/pokemongo/pokemon/002.png",
+  "size": {
+    "height": "0.99 m",
+    "weight": "13.0 kg"
+  },
+  "pokemon-rarity": "normal",
+  "type": [
+    "grass",
+    "poison"
+  ]
+},
+]
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const salida = [{
+  "num": "001",
+  "name": "bulbasaur",
+  "generation": {
+    "num": "generation i",
+    "name": "kanto"
+  },
+  "about": "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+  "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+  "size": {
+    "height": "0.71 m",
+    "weight": "6.9 kg"
+  },
+  "pokemon-rarity": "normal",
+  "type": [
+    "grass",
+    "poison"
+  ]
+},
+{
+  "num": "004",
+  "name": "charmander",
+  "generation": {
+    "num": "generation i",
+    "name": "kanto"
+  },
+  "about": "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.",
+  "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+  "size": {
+    "height": "0.61 m",
+    "weight": "8.5 kg"
+  },
+  "pokemon-rarity": "normal",
+  "type": [
+    "fire"
+  ]
+}, {
+  "num": "002",
+  "name": "ivysaur",
+  "generation": {
+    "num": "generation i",
+    "name": "kanto"
+  },
+  "about": "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
+  "img": "https://www.serebii.net/pokemongo/pokemon/002.png",
+  "size": {
+    "height": "0.99 m",
+    "weight": "13.0 kg"
+  },
+  "pokemon-rarity": "normal",
+  "type": [
+    "grass",
+    "poison"
+  ]
+},
+
+]
+
+const salidaAz = [
+  {
+    "num": "002",
+    "name": "ivysaur",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/002.png",
+    "size": {
+      "height": "0.99 m",
+      "weight": "13.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ]
+  },
+  {
+    "num": "004",
+    "name": "charmander",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+    "size": {
+      "height": "0.61 m",
+      "weight": "8.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ]
+  }, {
+    "num": "001",
+    "name": "bulbasaur",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "size": {
+      "height": "0.71 m",
+      "weight": "6.9 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ]
+  },
+]
+
+
+describe("Filtrado por tipo", () => {
+  it("Devuelve un array solo con los elementos del tipo especificado", () => {
+
+    // Datos de prueba
+    const data = [
+      { id: 1, type: "fire" },
+      { id: 2, type: "water" },
+      { id: 3, type: "grass" },
+      { id: 4, type: "fire" }
+    ];
+
+    //Tipo a filtrar
+    const tipo="fire"
+
+    expect(filtradoPorTipo(data, tipo)).toEqual([
+      { id: 1, type: "fire" },{ id: 4, type: "fire" }
+    ]);
+  });  
+});
+
+describe("ordenar", () => {
+  it("is an array", () => {
+    expect(typeof ordenar).toBe("array");
+  })
+  it("is a function", () => {
+    expect(typeof ordenar).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+  it("funcion entrega un array con nombres organizados de la A a la Z", () => {
+    expect(ordenar(entrada)).toEqual(salida)
+  })
+    
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe("ordenar de la Z a la A", () => {
+  it("is an array", () => {
+    expect(typeof ordenarZa()).toBe("array");
+  })
+  it("is a function", () => {
+    expect(typeof ordenarZa).toBe("function");
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+  it("funcion entrega un array con nombres organizados de la Z a la A", () => {
+    expect(ordenarZa(entrada)).toEqual(salidaAz)
+  })
 });
