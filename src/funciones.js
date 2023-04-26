248b9d3e87c5f65f6eb198dff7de48a4dd3a7d49
Mariana-Sanchez21 <marysansam21@gmail.com>
2023-04-26 09:06:44 -0500

    test del calculo y mejora del responsive

--- src/funciones.js
+++ src/funciones.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import data from './data/pokemon/pokemon.js';
 
 //filter
@@ -6,44 +7,56 @@ export const datos = data.pokemon;
 export const datosNombres = data.pokemon.name;
 
 
-export function filtrado(nombrePokemon){
-  const pokemons = datos.filter(pokemon => nombrePokemon  === pokemon.name);
-  if (pokemons.length === 0){
+export function filtrado(nombrePokemon) {
+  const pokemons = datos.filter(pokemon => nombrePokemon === pokemon.name);
+  if (pokemons.length === 0) {
     return "No se encontró";
-    
+
   } else {
     return pokemons;
   }
-  
+
 }
 const resultados = filtrado("hola");
 console.log(resultados)
 
 //Sort
 
-export function sortBy(datos, ordenPokemon){
-  return datos.sort((a,b) => {
-    if (ordenPokemon === "asc"){
-      if (a.name > b.name){
+export function sortBy(datos, ordenPokemon) {
+  return datos.sort((a, b) => {
+    if (ordenPokemon === "asc") {
+      if (a.name > b.name) {
         return 1;
-      } else if (a.name < b.name){
+      } else if (a.name < b.name) {
         return -1;
       }
     }
-    if (ordenPokemon === "desc"){
-      if (a.name < b.name){
+    if (ordenPokemon === "desc") {
+      if (a.name < b.name) {
         return 1;
-      } else if (a.name > b.name){
+      } else if (a.name > b.name) {
         return -1;
-      } 
+      }
     }
   });
 }
 const resultados2 = sortBy([
-  {"name": "charmander"},
-  {"name": "charmeleon"},
-  {"name": "bulbasaur"},
-  {"name": "hypno"},
-  {"name": "voltorb"}
+  { "name": "charmander" },
+  { "name": "charmeleon" },
+  { "name": "bulbasaur" },
+  { "name": "hypno" },
+  { "name": "voltorb" }
 ], "desc")
 console.log(resultados2)
+
+//calculo agregado
+
+export function contarRol(datos, rol) {
+  let count = 0;
+  for (let i = 0; i < datos.length; i++) {
+    if (datos[i].includes(rol)) {
+      count++;
+    }
+  }
+  return ((count * 100) / datos.length).toFixed(2) + '%';
+}
