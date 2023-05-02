48b26bb605dd0486f3f9f2dcef4d280cce4bac3b
Dul <dul31389@gmail.com>
2023-05-01 23:36:20 -0500

    responsive

--- src/saludo.js
+++ src/saludo.js
@@ -1,21 +1,19 @@
 //crear la funcion llamada transicion
 //seleccionar el div dialogo1 x su id y decirle que se debe ocultar con el none
-//despues llamar al otro div dialogo2 x su id y decirle que debe mostrarse en pantalla 
+//despues llamar al otro div dialogo2 x su id y decirle que debe mostrarse en pantalla
 //todo esto ocurrira dentro de 3segundos
 function transicion() {
   document.getElementById("dialogo1").style.display="none";
   document.getElementById("dialogo2").style.display="flex";
 }
-  
+
 setTimeout(transicion, 4000);
-  
-  
-  
-  
+
+
+
+
 //de pagina saludo al index
 const siguiente = document.getElementById('boton-next-instrucc');
 siguiente.addEventListener('click', () => {
   window.location.href ="main.html";
 });
-  
-  
