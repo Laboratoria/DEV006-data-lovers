db261437e44d400eddf1503854948129f92f1c29
Dul <dul31389@gmail.com>
2023-05-04 00:06:48 -0500

    responsive saludo

--- src/saludo.js
+++ src/saludo.js
@@ -1,33 +1,34 @@
 //crear la funcion llamada transicion
 //seleccionar el div dialogo1 x su id y decirle que se debe ocultar con el none
 //despues llamar al otro div dialogo2 x su id y decirle que debe mostrarse en pantalla
-//todo esto ocurrira dentro de 3segundos
+//todo esto ocurrira dentro de 5segundos
 //PANTALLA PC
 function transicion() {
   document.getElementById("dialogo1").style.display="none";
   document.getElementById("dialogo2").style.display="flex";
 }
-setTimeout(transicion, 4000);
+setTimeout(transicion, 5000);
 
 
 //PANTALLA CEL
 function transicionCel(){
   document.getElementById("dialogo3").style.display="none";
-  document.getElementById("intrucc3").style.display="flex";
+  document.getElementById("instrucc3").style.display="flex";
 }
-setTimeout(transicionCel, 6000);
+setTimeout(transicionCel, 5000);
 
 
 //PANTALLA TABLET
 function transicionTablet(){
   document.getElementById("dialogo4").style.display="none";
-  document.getElementById("intrucc4").style.display="flex";
+  document.getElementById("instrucc4").style.display="flex";
 }
 setTimeout(transicionTablet, 5000);
 
 
 //de pagina saludo al index
 const siguiente = document.getElementById('boton-next-instrucc');
-siguiente.addEventListener('click', () => {
+siguiente.addEventListener('click', (event) => {
+  event.preventDefault();
   window.location.href ="main.html";
 });
